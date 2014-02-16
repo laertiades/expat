d1d40b86fc4e9065960fbcdc3c6b97576c1bf6e3
Jesse Gall <jesse@goodfellowsites.com>
2014-02-15 19:20:07 -0800

    error restore

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,9 @@
 class ErrorsController < ApplicationController
   def not_found
-    render template: 'errors/not_found', layout: 'layouts/application', status: 404
+    respond_to do |format|
+      format.html { render template: 'errors/not_found', layout: 'layouts/application', status: 404 }
+      format.all  { render nothing: true, status: 404 }
+    end
   end
 
   def unacceptable
