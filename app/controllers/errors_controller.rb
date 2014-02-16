9a1b7e43cef11b9a3fb7fead89ca3fc535711525
Jesse Gall <jesse@goodfellowsites.com>
2014-02-15 19:54:16 -0800

    error pages complete

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -4,16 +4,10 @@ class ErrorsController < ApplicationController
   end
 
   def unacceptable
-    respond_to do |format|
-      format.html { render template: 'errors/unacceptable', layout: 'layouts/application', status: 422 }
-      format.all  { render nothing: true, status: 422 }
-    end
+      render :status => 422
   end
 
   def internal_error
-    respond_to do |format|
-      format.html { render template: 'errors/internal_error', layout: false, status: 500 }
-      format.all  { render nothing: true, status: 500}
-    end
+      render :layout => false, :status => 500
   end
 end
