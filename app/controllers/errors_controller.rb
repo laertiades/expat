34365b208c86bd0cbf549f06a22cb9e0e89213b4
Jesse Gall <jesse@goodfellowsites.com>
2014-02-17 11:16:03 -0800

    mime types

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,22 @@
 class ErrorsController < ApplicationController
   def not_found
-      render :status => 404, :layout => "error_frame"
+    respond_to do |format|
+      format.any(:htm, :html, :xls, :xlsx) { render :status => 404, :layout => "error_frame", :formats => [:html] }
+      format.all  { render nothing: true, status: 404 }
+    end
   end
 
   def unacceptable
-      render :status => 422, :layout => "error_frame"
+    respond_to do |format|
+      format.html { render :status => 422, :layout => "error_frame" }
+      format.all  { render nothing: true, status: 422 }
+    end
   end
 
   def internal_error
-      render :layout => false, :status => 500
+    respond_to do |format|
+      format.html { render :layout => false, :status => 500 }
+      format.all  { render nothing: true, status: 500}
+    end
   end
 end
