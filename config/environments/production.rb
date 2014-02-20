a5ce924560984bd94398a8e861cd78378a9f1b09
Jesse Gall <jesse@goodfellowsites.com>
2014-02-19 19:25:41 -0800

    mobile css in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Expat::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.precompile += %w(mobapp.css)
   # Generate digests for assets URLs.
   config.assets.digest = true
 
