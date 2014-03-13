e142276732394fda70f6771f809dfb4b8724ceb0
Jesse Gall <jesse@goodfellowsites.com>
2014-03-12 18:16:26 -0700

    remove title header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Expat CPA"
+    base_title = "Expat CPA | American Expatriate Tax Services"
     if page_title.empty?
       base_title
     else
-      "#{base_title} | #{page_title}"
+      "#{page_title}"
     end
   end
 
--- config/initializers/bust_cache.rb
+++ config/initializers/bust_cache.rb
@@ -1,2 +1,2 @@
 # creates new seed for creation of etag in header
-ENV["RAILS_CACHE_ID"] = 'v000013'
+ENV["RAILS_CACHE_ID"] = 'v000014'
